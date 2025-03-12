e0de64dfc4080a00253f42edddc6cf37e65087d5
wkUvMYRXKwUjSdDUSfR5Tlih9MzXEAkR-FnvHg3 <mdnobz3+1@gmail.com>
2025-03-12 03:20:22 +0000

    Completed User Registration and Spring Test

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,24 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/register")
+    public ResponseEntity postUserRegistration(@RequestBody Account account) {
+        if (account.getUsername().isBlank() || account.getPassword().length() < 4) {
+            return ResponseEntity.status(400).body("Client error");
+        }
+        if (accountService.findByUsername(account.getUsername()) != null) {
+            return ResponseEntity.status(409).body("Conflict");
+        }
+        Account newAccount = accountService.addAccount(account);
+        return ResponseEntity.status(200).body(newAccount);
+    }
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,21 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Account findByUsername(String username) {
+        return accountRepository.findByUsername(username);
+    }
+
+    public Account addAccount(Account account){
+        return accountRepository.save(account);
+    }
 }
